ffd9cc84dc3cd00f1c1fd47f9b69e28f45b603b5
Alina Hassan <161017071+AlinaHassan114@users.noreply.github.com>
2024-02-26 13:46:31 +0500

    Update Hello.java
    changed code

--- Hello.java
+++ Hello.java
@@ -1,8 +1,13 @@
 public class Hello {
  public static void main (String args[])  {
-    int num=89;
-    System.out.println(num);
+    int num1 =89;
+  int num2 = 90;
+  System.out.println(num1);
+  System.out.println(num2);
   System.out.println("Hello World!!!!");
+  
+  
  } 
 }
 // this is the first test
+// this is the second test
