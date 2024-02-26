e8231b21c2183354da20dd62de7db60e1a05db30
Vaheebah <161125470+vaheebah@users.noreply.github.com>
2024-02-26 13:40:06 +0500

    Update Hello.java
    changed your code  mate

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
  public static void main (String args[])  {
     int num=89;
     System.out.println(num);
+  System.out.println("Hello World!!!!");
  } 
 }
 // this is the first test
